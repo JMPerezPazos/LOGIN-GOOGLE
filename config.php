f47b59bce87e7e77d47a16b6f8f8523e843890d4
JMPerezPazos <juanmagames2008@gmail.com>
2025-06-05 18:53:21 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,6 @@
 <?php
     require_once 'vendor/autoload.php';
-    
-    $clientID= '543600671002-uhssfsdgk879oenqv03s3pmkcok567j5.apps.googleusercontent.com';
-    $clientSecret ='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';
-    $redirectUri = 'http://localhost/TP_LOGIN/auten.php';
-
+   
     //creamos un cliente
     $client = new Google_Client();
     $client ->setClientId($clientID);
